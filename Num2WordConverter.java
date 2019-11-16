b2ebd68a32c34ac82d8e9666f0f3b1676ef70a6e
Revanth <revanthkt@virtusa.com>
2019-11-16 08:13:26 +0530

    c

---
+++ Num2WordConverter.java
@@ -0,0 +1,13 @@
+/* Number to Word Convertor */
+public class Num2WordConverter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i =1000; //taken a number
+		if(i==1000)
+		{
+			System.out.println("One thousand");
+		}
+	}
+	
+}
